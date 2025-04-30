e378ad29e7abbefce7e13ebdf73246732b0012c6
iceee404 <67895852+iceee404@users.noreply.github.com>
2025-04-30 21:19:41 +0800

    upload basic project structure

--- helpers/parse.py
+++
@@ -1,5 +0,0 @@
-def parse_amount(value):
-    try:
-        return float(str(value).replace('$', '').replace(',', ''))
-    except ValueError:
-        return None
--- static/js/index.js
+++
@@ -1,5 +0,0 @@
-function uploadFile() {
-    const file = document.getElementById('fileElem').files[0];
-    const formData = new FormData();
-    formData.append('pdf', file);
-}
